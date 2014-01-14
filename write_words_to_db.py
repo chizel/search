054edfdff77640d7d4716afe1c00e85fac385135
Denis Gatin <chizel@inbox.ru>
2014-01-14 22:13:48 +0200

    add console management

--- write_words_to_db.py
+++ write_words_to_db.py
@@ -1,16 +1,11 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import psycopg2
-import codecs
+import psycopg2, codecs, sys, getopt
 
-DB_NAME = 'pdb'
-TABLE_NAME = 'useless_words'
-FILE_NAME = 'useless_words.txt'
-
-def read_words():
+def read_words(file_name):
     try:
-        f = codecs.open(FILE_NAME, 'r', 'utf-8')
+        f = codecs.open(file_name, 'r', 'utf-8')
         data = f.read()
         f.close()
     except IOError:
@@ -26,14 +21,33 @@ def read_words():
     words.sort()
     return words
 
+def main(argv):
+    DB_NAME = 'pdb'
+    TABLE_NAME = 'useless_words'
+    FILE_NAME = 'useless_words.txt'
 
-def main():
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print 'test.py -f <inputfile> -t <table_name> -d <database_name>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -f <inputfile> -t <table_name> -d <database_name>'
+            sys.exit()
+        elif opt == "-f":
+            FILE_NAME = arg
+        elif opt == "-d":
+            DB_NAME = arg
+        elif opt == "-t":
+            TABLE_NAME = arg
+
     conn_string = "host='localhost' dbname='" + DB_NAME + "' user='fordb' password='qwerty'"
 
     try:
         conn = psycopg2.connect(conn_string)
         cursor = conn.cursor()
-        words = read_words()
+        words = read_words(FILE_NAME)
 
         for word in words:
             cursor.execute("""INSERT INTO %s VALUES ('%s');""" % (TABLE_NAME, word))
@@ -41,10 +55,9 @@ def main():
         conn.commit()
         cursor.close()
         conn.close()
+
     except Exception , e:
         print 'ERROR:', e[0]
-    return
-
 
 if __name__ == "__main__":
-    main()
+   main(sys.argv[1:])
